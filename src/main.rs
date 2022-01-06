e25a4bd8f46c4bc6732259f69f8115ed54d8498e
Lucas Mol Leite <lucas.leite@globalhitss.com.br>
2022-01-06 13:27:05 -0300

    First commit of exercise 1 of numbers teory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    use std::io;
+
+    fn main() {
+        //let mut vet = vec![];
+        //vet[0] = 20;
+        //let mut v: [i32; 5] = [1, 2, 3, 4, 5];
+        //v[3] = 10;
+        let mut input = String::new();
+        let mut maxinteration = String::new();
+        io::stdin().read_line(&mut maxinteration).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+
+        for i in 0..maxinteration.parse::<i32>().unwrap() {
+            println!("Is the number Correct? {}", input);
+        }
+    }
 }
