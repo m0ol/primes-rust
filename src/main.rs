d586b6a4c378a8dc3d5249d2c236bb8456cb7de0
Lucas Mol Leite <lucas_mol@hotmail.com>
2022-01-09 11:07:05 -0300

    Basis for the first exercise

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use std::io;
+
 fn main() {
-    use std::io;
+    //let mut vet = vec![];
+    //vet[0] = 20;
+    //let mut v: [i32; 5] = [1, 2, 3, 4, 5];
+    //v[3] = 10;
+    let mut input = String::new();
+    let mut maxinteration = String::new();
+    let mut interation: i32;
+    let mut numbers: Vec<&str>;
+    io::stdin().read_line(&mut maxinteration).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
 
-    fn main() {
-        //let mut vet = vec![];
-        //vet[0] = 20;
-        //let mut v: [i32; 5] = [1, 2, 3, 4, 5];
-        //v[3] = 10;
-        let mut input = String::new();
-        let mut maxinteration = String::new();
-        io::stdin().read_line(&mut maxinteration).unwrap();
-        io::stdin().read_line(&mut input).unwrap();
+    interation = maxinteration.trim().parse::<i32>().unwrap();
+    numbers = input.split(" ").collect();
 
-        for i in 0..maxinteration.parse::<i32>().unwrap() {
-            println!("Is the number Correct? {}", input);
-        }
+    for i in 0..interation {
+        println!("Is the number Correct? {}", numbers[0]);
+        println!("Is the number Correct2? {}", numbers[1]);
     }
 }
