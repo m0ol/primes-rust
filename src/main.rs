a5e63e2281c00b0b639e4e165ded54eeee03c8e8
Lucas Mol Leite <lucas_mol@hotmail.com>
2022-01-11 17:30:23 -0300

    Finish with wrong output format

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod prime_check {
     pub fn prime_checkfn(n: i32) -> bool {
         let mut i: i32 = 2;
         let mut is_prime: bool = true;
+        if n == 1 {
+            is_prime = false;
+        }
         while i < n {
             if n % i == 0 {
                 is_prime = false;
@@ -17,31 +20,40 @@ mod prime_check {
 
     pub fn user_input_primes() -> Vec<i32> {
         let mut input = String::new();
-        let mut numbers: Vec<&str>;
+        let numbers: Vec<&str>;
         let mut numbersprimes: Vec<i32> = Vec::new();
         io::stdin().read_line(&mut input).unwrap();
         numbers = input.split(" ").collect();
+        let mut i: i32 = numbers[0].trim().parse::<i32>().unwrap();
+        while i <= numbers[1].trim().parse::<i32>().unwrap() {
+            if prime_checkfn(i) {
+                numbersprimes.push(i);
+            }
+            i += 1;
+        }
+        return numbersprimes;
+
+        /*
+        Nao entendi o porque nao funcionou com for
         for _j in
             numbers[0].trim().parse::<i32>().unwrap()..numbers[1].trim().parse::<i32>().unwrap()
         {
             if prime_checkfn(_j) {
                 numbersprimes.push(_j);
-            } else {
-                continue;
             }
         }
-        return numbersprimes;
+        return numbersprimes;*/
     }
 }
 
 pub fn main() {
     let mut maxinteration = String::new();
     let interation: i32;
-    let mut numbersprimes: Vec<i32> = Vec::new();
+    let mut finalresult: Vec<Vec<i32>> = Vec::new();
     io::stdin().read_line(&mut maxinteration).unwrap();
     interation = maxinteration.trim().parse::<i32>().unwrap();
     for _i in 0..interation {
-        prime_check::user_input_primes();
+        finalresult.push(prime_check::user_input_primes());
     }
-    println!("{:?}", numbersprimes);
+    println!("{:?}", finalresult);
 }
