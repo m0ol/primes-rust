c1fe46202760b4876bac3a5dacebf48ba50d59bd
Lucas Mol Leite <lucas_mol@hotmail.com>
2022-01-12 16:51:37 -0300

    Just need to set the output

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::io;
 
 mod prime_check {
     use std::io;
-    pub fn prime_checkfn(n: i32) -> bool {
-        let mut i: i32 = 2;
+    pub fn prime_checkfn(n: i64) -> bool {
+        let mut i: i64 = 2;
         let mut is_prime: bool = true;
-        if n == 1 {
+        if n == 1 || n == 0 {
             is_prime = false;
         }
         while i < n {
@@ -18,14 +18,24 @@ mod prime_check {
         is_prime
     }
 
-    pub fn user_input_primes() -> Vec<i32> {
+    pub fn user_input_primes() -> Vec<i64> {
         let mut input = String::new();
         let numbers: Vec<&str>;
-        let mut numbersprimes: Vec<i32> = Vec::new();
+        let mut numbersprimes: Vec<i64> = Vec::new();
         io::stdin().read_line(&mut input).unwrap();
         numbers = input.split(" ").collect();
-        let mut i: i32 = numbers[0].trim().parse::<i32>().unwrap();
-        while i <= numbers[1].trim().parse::<i32>().unwrap() {
+        if numbers[0].trim().parse::<i64>().unwrap() < 1
+            || numbers[1].trim().parse::<i64>().unwrap() > 1000000000
+            || numbers[0].trim().parse::<i64>().unwrap() - numbers[1].trim().parse::<i64>().unwrap()
+                > 100000
+        {
+            println!(
+                "Please enter numbers between 1 and 1000000000 and with a difference of 100000"
+            );
+            return user_input_primes();
+        }
+        let mut i: i64 = numbers[0].trim().parse::<i64>().unwrap();
+        while i <= numbers[1].trim().parse::<i64>().unwrap() {
             if prime_checkfn(i) {
                 numbersprimes.push(i);
             }
@@ -37,10 +47,14 @@ mod prime_check {
 
 pub fn main() {
     let mut maxinteration = String::new();
-    let interation: i32;
-    let mut finalresult: Vec<Vec<i32>> = Vec::new();
+    let interation: i64;
+    let mut finalresult: Vec<Vec<i64>> = Vec::new();
     io::stdin().read_line(&mut maxinteration).unwrap();
-    interation = maxinteration.trim().parse::<i32>().unwrap();
+    if maxinteration.trim().parse::<i64>().unwrap() > 10 {
+        println!("Please enter a number less than 10");
+        return main();
+    }
+    interation = maxinteration.trim().parse::<i64>().unwrap();
     for _i in 0..interation {
         finalresult.push(prime_check::user_input_primes());
     }
